d2ed118ba79855906ed7881bb51c62150b51f805
Benjimin <omniception@gmail.com>
2018-01-23 10:28:15 -0600

    Wiring up buttons and learning about ScrollView

--- app/src/main/java/com/ancientarchive/timeline/MainActivity.java
+++ app/src/main/java/com/ancientarchive/timeline/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     private Button mTimelineButton;
     private Button mSettingsButton;
     private Button mToolsButton;
+    private Button mEventPreviewButton;
+
+    private HorizontalScrollView mTimelineView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +40,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Test toast for Tools Button", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mEventPreviewButton = (Button) findViewById(R.id.eventPreviewButton);
+        mEventPreviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Test toast for Timeline Button", Toast.LENGTH_SHORT).show();
+                mEventPreviewButton.setText("Ideally, clicking an Event in the main view will place its name here.");
+            }
+        });
+
+        mTimelineView = (HorizontalScrollView) findViewById(R.id.horizontalScrollView);
+        mTimelineView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Test toast for Timeline View Box. No content to scroll yet!", Toast.LENGTH_SHORT).show();
             }
         });
     }
